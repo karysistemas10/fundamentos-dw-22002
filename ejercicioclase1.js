25446f9f64637ae54fbd1cef5177f897df9929cb
karymartinez10 <karysistemas10@gmail.com>
2022-08-31 19:04:20 -0500

    Se modificaron archivos, subiendo a git

---
+++ ejercicioclase1.js
@@ -0,0 +1,121 @@
+// console.log('Hello world')
+
+// let deporte = 'Atletismo';
+// let saludo = 'Buenos días!';
+// let perro = 'Firulais';
+// let pais = 'México';
+// let edad = 53;
+
+// let nombreDeMiPerro = 'Lucas'
+// let animalVerde = 'Iguana'
+// let impuestosAnuales = 22334455
+
+// Tipos de datos
+
+// let valorBooleano = true;
+// let objetoPrueba = {
+//     propiedad: 'Mio',
+//     perro: 'Lucas'
+// }
+// String
+// let a = 'Let\'s learn Javascript'
+// let b = 'He said "Hello world"'
+
+// Number
+// let a = 12
+// let b = 32.43
+// let c = 26.34e+6
+// let d = 26.34e-6
+
+// console.log(7 / 0)  // INfinity
+// console.log(-7 / 0)  // -INfinity
+// console.log(Math.sqrt(-1)) // NaN
+// console.log('Hello world' / 2) //NaN
+// let isUserActive = true;
+
+// let a;
+// let b = 'Hello world'
+
+// a = null;
+
+// // Numbers
+// typeof 10;	// "number"
+// typeof 12.5;	// "number"
+// typeof 2.5e+6;	// "number"
+// typeof Infinity;	// "number"
+// typeof NaN;	// "number"
+
+// // Strings
+// typeof ' ';	// "string" - Espacios en blanco cuentan como caracteres
+// typeof '';
+// typeof 'Hello World';	// "string"
+// typeof '12';	// "string" - Números dentro de comillas son strings
+
+// // Booleans
+// typeof true;	// "boolean"
+// typeof false;	// "boolean"
+
+// // Undefined
+// typeof undefined;	// "undefined"
+
+// var name;
+// typeof name; // "undefined" - Aún no se asigna un valor
+// typeof Null;	// "undefined"
+
+// // Null
+// typeof null;
+
+// console.log(String(123)) // Explicita
+
+// 123 + '' // implicita
+
+// Number('123') // Explicita
+// + '123' // Implicita
+// 1 - '1'
+// 2 * '2'
+
+// Number(' 10 ') // 10
+// Number(10 + '  ') // 10
+// Number(10 + '..') // NAN
+
+// let number = 10;
+// if (!!undefined) {console.log('Hello world')}
+// if (typeof number === 'number') {console.log('Hello world 2')}
+
+let a = 5;
+let b = 10;
+let c = 15;
+
+// a + b;  // 15
+
+// b - a;  // 5
+
+// a * c;  // 75
+
+// c / a;  // 3
+
+// b % a;  // 0
+
+// a + b * c // 155
+
+// (a + b) * c // 225
+
+// b / a * c // 30
+
+// b / (a * c) // 0.13333
+
+// a - b + c;
+
+// a - (b + c);
+
+// (3 + b) * c / a * 2; //78
+
+// ((a + b * c) / 5) * 2; // 62
+
+// b / a + 2 * c; //32
+
+// (a + b + c / c) * a; // 80
+
+// ((3 * a) / c) + a + b + c; // 31
+
+// a - (b + c) * a / 1; // -120
